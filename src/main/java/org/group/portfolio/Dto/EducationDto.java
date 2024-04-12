26cf8b995f8404d411f15bb48a1aa9d31c644fd5
walid habbach <walid.habbach@gmail.com>
2024-04-12 17:28:21 +0000

    Update Dto  Education , Experience

--- src/main/java/org/group/portfolio/Dto/EducationDto.java
+++ src/main/java/org/group/portfolio/Dto/EducationDto.java
@@ -5,14 +5,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.group.portfolio.Entities.Education;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EducationDto {
     private String institution;
     private String degree;
-    private int startYear;
-    private int endYear;
+    private Date startDate;
+    private Date endDate;
     private String fieldOfStudy;
     private String location;
     private String description;

061b3201646fe32477bbbea03cadc7a964b1531f
walid habbach <walid.habbach@gmail.com>
2024-04-17 21:06:58 +0100

    update Controllers and mapping
    update mapping of experience
    and project
    update controllers experience ,project , education

--- src/main/java/org/group/portfolio/Entities/Education.java
+++ src/main/java/org/group/portfolio/Entities/Education.java
@@ -24,4 +24,3 @@ public class Education {
     private String location;
     private String description;
 }
-
--- src/main/java/org/group/portfolio/Service/Interfaces/ExperienceService.java
+++ src/main/java/org/group/portfolio/Service/Interfaces/ExperienceService.java
@@ -3,7 +3,7 @@ package org.group.portfolio.Service.Interfaces;
 import org.group.portfolio.Dto.ExperienceDto;
 import org.group.portfolio.Entities.Experience;
 
-public interface IExperienceService {
+public interface ExperienceService {
     public Experience GetById(String id);
     public Experience Create(ExperienceDto experienceDto);
     public Experience Update(String id, ExperienceDto experienceDto);
